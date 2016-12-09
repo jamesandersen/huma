53e85ac23b168d3de742c692ec68257ca72815ee
James Andersen <james.andersen@universalmind.com>
2016-12-08 23:40:13 -0700

    Layout and state enhancement

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{431:function(n,o){},980:function(n,o,c){n.exports=c(431)}},[980]);
+webpackJsonp([1,2],{432:function(n,o){},983:function(n,o,c){n.exports=c(432)}},[983]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
